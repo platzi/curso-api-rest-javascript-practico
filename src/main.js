4444443cb0b0302b0f2d6663c8017f3ad82df128
Juan David Castro <me@juandc.co>
2022-03-03 11:20:26 -0500

    Clase 06: categories preview

--- src/main.js
+++ src/main.js
@@ -22,4 +22,27 @@ async function getTrendingMoviesPreview() {
   });
 }
 
+async function getCategegoriesPreview() {
+  const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+  const data = await res.json();
+
+  const categories = data.genres;
+  categories.forEach(category => {
+    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+    
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category-container');
+
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.classList.add('category-title');
+    categoryTitle.setAttribute('id', 'id' + category.id);
+    const categoryTitleText = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    previewCategoriesContainer.appendChild(categoryContainer);
+  });
+}
+
 getTrendingMoviesPreview();
+getCategegoriesPreview();
